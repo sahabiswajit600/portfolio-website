d08188e85f003e158aea6886d2d3ee7dd0b22131
Biswajit Saha <sahabiswajit600@gmail.com>
2022-09-21 00:19:12 +0530

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Biswajit Saha</h1>
+                <h5 className="text-light">Full Stack Developer</h5>
+                <CTA />
+
+                <div className="me">
+                    <img src={ME} alt="" />
+                </div>
+
+                <a href="#contact" className="scroll__down">Scroll Down</a>
+            </div>
+        </header>
     )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className="header__socials">
+            <a href="https://www.linkedin.com/in/sahabiswajit600/" target="_blank"></a>
+            <a href="https://github.com/sahabiswajit600" target="_blank"></a>
+            <a href="https://dribbble.com/sahabiswajit600" target="_blank"></a>
+        </div>
+    )
+}
+
+export default HeaderSocials
