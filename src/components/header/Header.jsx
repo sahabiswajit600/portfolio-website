4b72ccca83fec005ab7c380bbbb6ad9a9695d769
Biswajit Saha <sahabiswajit600@gmail.com>
2022-10-14 22:07:10 +0530

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+import { ChakraProvider, Image, Box } from '@chakra-ui/react'
 
 function Header() {
     return (
@@ -11,10 +13,16 @@ function Header() {
                 <h1>Biswajit Saha</h1>
                 <h5 className="text-light">Full Stack Developer</h5>
                 <CTA />
+                <HeaderSocials />
 
-                <div className="me">
+                {/* <div className="me">
                     <img src={ME} alt="" />
-                </div>
+                </div> */}
+                <Image className="me"
+                    boxSize='200px'
+                    src="https://i.ibb.co/cwHqZZV/image.png"
+                    alt='Biswajit Saha'
+                />
 
                 <a href="#contact" className="scroll__down">Scroll Down</a>
             </div>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { FiDribbble } from 'react-icons/fi'
+import { BsFacebook } from 'react-icons/bs'
+import { FaInstagramSquare } from 'react-icons/fa'
 
 const HeaderSocials = () => {
     return (
         <div className="header__socials">
-            <a href="https://www.linkedin.com/in/sahabiswajit600/" target="_blank"></a>
-            <a href="https://github.com/sahabiswajit600" target="_blank"></a>
-            <a href="https://dribbble.com/sahabiswajit600" target="_blank"></a>
+            <a href="https://www.linkedin.com/in/sahabiswajit600/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
+            <a href="https://github.com/sahabiswajit600" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+            <a href="https://dribbble.com/sahabiswajit600" target="_blank" rel="noopener noreferrer"><FiDribbble /></a>
+            <a href="https://www.facebook.com/sahabiswajit600" target="_blank" rel="noopener noreferrer"><BsFacebook /></a>
+            <a href="https://www.instagram.com/sahabiswajit600/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare /></a>
         </div>
     )
 }
